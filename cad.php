a0f60392cf41a86e2a6caff060aa47a67f0b2478
fabioLoterio <vitorloterio@gmail.com>
2019-11-28 07:59:05 -0300

    little changes

--- cad.php
+++ cad.php
@@ -19,27 +19,60 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/materialize.css">
 	<title><?php echo $title; ?></title>
 </head>
 <body>
-<br>
-<a href="index.php"><button>Listar</button></a>
-<a href="cad.php"><button>Novo</button></a>
+
+<a href="index.php" class='btn'>Listar</a>
+<a href="cad.php" class='btn'>Novo</a>
+
 	<div class="container">
 		<form action="acao.php" method="post"><!-- notes(codigo,titulo,texto,corFundo,tags,ativa,estrela) -->
-			<input readonly type="text" name="codigo" id="codigo" value="<?php if ($acao == "editar") echo $dados['codigo']; else echo 0; ?>"><br>
-			<input required=true placeholder='titulo' type="text" name="titulo" id="titulo" value="<?php if ($acao == "editar") echo $dados['titulo']; ?>"><br>
-			<textarea required=true placeholder='text' type="text" name="texto" id="texto" cols="20" rows="10"><?php if ($acao == "editar") echo $dados['texto']; ?></textarea><br>
-			<input type="color" name="corFundo" id="corFundo" value="<?php if ($acao == "editar") echo $dados['corFundo']; ?>"><br>
-			<input required=true placeholder='tags' type="text" name="tags" id="tags" value="<?php if ($acao == "editar") echo $dados['tags']; ?>"><br>
-			ativa
-			<input type="checkbox" name="ativa" value='1' <?php if($acao == "editar" && $dados['ativa'] == 1) echo "checked";?>><br>
-			estrela
-			<input type="checkbox" name="estrela" value='1' <?php if($acao == "editar" && $dados['estrela'] == 1) echo "checked";?>><br>
 
-			<button type="submit" name="acao" id="acao" value="salvar">Salvar</button>
+			<div class="input-field col s2">
+				<input readonly type="text" name="codigo" id="codigo" value="<?php if ($acao == "editar") echo $dados['codigo']; else echo 0; ?>" class="validate">
+				<label for="codigo">Código</label>
+			</div>
+
+			<div class="input-field col s6">
+				<input required=true placeholder='titulo' type="text" name="titulo" id="titulo" value="<?php if ($acao == "editar") echo $dados['titulo']; ?>" class="validate">
+				<label for="titulo">Titulo</label>
+			</div>
+
+			<div class="input-field col s2">
+				<input type="text" name="texto" id="texto" value="<?php if ($acao == "editar") echo $dados['texto']; else echo 0; ?>" class="validate">
+				<label for="codigo">Texto</label>
+			</div>
+
+			<div class="input-field col s12">
+        <textarea class="materialize-textarea" required=true placeholder='text' type="text" name="texto" id="texto" cols="20" rows="10" class='validate'><?php if ($acao == "editar") echo $dados['texto']; ?></textarea>
+        <label for="texto">Textarea</label>
+      </div>
+
+			<input type="color" name="corFundo" id="corFundo" value="<?php if ($acao == "editar") echo $dados['corFundo']; ?>"><br>
+
+			<input required=true placeholder='tags' type="text" name="tags" id="tags" value="<?php if ($acao == "editar") echo $dados['tags']; ?>"><br>
+
+			<p>
+				<label>
+					<input type="checkbox" name="ativa" value='1' <?php if($acao == "editar" && $dados['ativa'] == 1) echo "checked";?>/>
+					<span>Ativa</span>
+				</label>
+			</p>
+			<p>
+				<label>
+					<input type="checkbox" name="estrela" value='1' <?php if($acao == "editar" && $dados['estrela'] == 1) echo "checked";?>/>
+					<span>Estrela</span>
+				</label>
+			</p>
+
+			<button type="submit" class='btn' name="acao" id="acao" value="salvar">Salvar</button>
 		</form>
 	</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -6,6 +6,8 @@
 
 	$title = "Google Keep";
 	
+	$busca = isset($_POST['search']) ? $_POST['search'] : null;
+	
 ?>
 <html lang="en">
 <head>
@@ -14,47 +16,47 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/materialize.css">
 	<title><?php echo $title; ?></title>
 </head>
 <body>
+	<a href="cad.php" class='btn'>novo</a>
 	<div class="container">
-		<a href="cad.php"><button>Novo</button></a>
-		<form action="" method="post">
-			<input type="text" placeholder='Digite sua busca'>
-			<input type="submit" value="Buscar">	
+		<form method="post">
+			<input type="text" placeholder='Digite sua busca' name='search'>
+			<input type="submit" class='btn center'	value="Buscar">	
 		</form>
 		<?php  	
 			$pdo = Conexao::getInstance();
 			$consulta = $pdo->query("SELECT * FROM notes;");
 			while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) { 
-		?>			
-		<div class="card" style="background-color: <?php echo $linha['corFundo'];?>">
-			<div class="container">
-				<div class="card-head"> 
-					<h4><b><?php echo "#" . $linha['codigo'] . " | " . $linha['titulo'];?></b></h4>
+		?>
+
+		<div class="row">
+			<div class="col s12 m6">
+				<div class="card" style="background-color: <?php echo $linha['corFundo'];?>">
+					<div class="card-content white-text">
+						<span class="card-title"><?php echo "#" . $linha['codigo'] . " | " . $linha['titulo'];?></span>
+						<p><?php echo $linha['texto'];?></p>
+						<p><?php echo $linha['tags'];?></p>
+						<p>
+							<?php 
+								if($linha['ativa'] == 1) echo "Ativa: sim | "; else	echo "Ativa: não | ";
+								if($linha['estrela'] == 1) echo "Estrela: sim";	else echo "Estrela: não";
+							?>	
+						</p>
+					</div>
+					<div class="card-action">
+						<a href='cad.php?acao=editar&codigo=<?php echo $linha['codigo'];?>'><i class="material-icons left">create</i></a>
+						<a href="javascript:excluirRegistro('acao.php?acao=excluir&codigo=<?php echo $linha['codigo'];?>')"><i class="material-icons left">delete</i></a>
+					</div>
 				</div>
-				<div class="card-body">
-					<p>
-						<?php echo $linha['texto'];?>
-					</p>
-					<p>
-						<?php echo $linha['tags'];?>
-					</p>
-				</div>
-				<div class="card-bottom">
-					<p>
-						<?php 
-							if($linha['ativa'] == 1) echo "Ativa: sim | "; else	echo "Ativa: não | ";
-							if($linha['estrela'] == 1) echo "Estrela: sim";	else echo "Estrela: não";
-						?>	
-					</p>
-					<a href='cad.php?acao=editar&codigo=<?php echo $linha['codigo'];?>'>/\</a>
-					<a href="javascript:excluirRegistro('acao.php?acao=excluir&codigo=<?php echo $linha['codigo'];?>')">X</a>
-				</div>			
 			</div>
-		</div> 
+		</div>
+		
     <?php } // fecha o while ?>  
 	</div> 
+
 <script>
 	function excluirRegistro(url) {
 		if (confirm("Confirmar Exclusão?"))
